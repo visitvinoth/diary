91c8ce6e9f4ef479aaec5a45af0370cee9a5213f
Ashwin <ashwin@Ashwin-PC.(none)>
2013-10-23 05:05:45 -0400

    migration comma extra

--- db/migrate/20131022150359_create_posts.rb
+++ db/migrate/20131022150359_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.datetime, :entry_date
-      t.text, :entry_text
+      t.datetime :entry_date
+      t.text :entry_text
       t.integer :user_id
       t.timestamps
     end
